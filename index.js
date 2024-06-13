02fbef61843846412e77216b845f40c2e289ae01
Godamit <shivabhai461@gmail.com>
2024-06-13 18:11:50 +0530

    media

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+$('.profile').hide();
+
+$('#i5').on('click',()=>{
+    // $('.profile').css('background-color','blue');
+    $('.profile').show();
+});
+$('#X').on('click',()=>{
+    // $('.profile').css('background-color','blue');
+    $('.profile').hide();
+});
+
+$('#im5').on('click',()=>{
+    // $('.profile').css('background-color','blue');
+    $('.profile').show();
+});
+
